bec112346c051f3f39901681508e2436e3ba4364
Martin Sherburn <martin.sherburn@datapath.co.uk>
2015-10-14 15:51:52 +0100

    Added batch file to build live555 on windows

---
+++ build_live.bat
@@ -0,0 +1,34 @@
+@echo off
+
+title build_live
+
+:: Change this to the correct location.
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+
+:::::::::::::::::::::::::::::::::::::
+
+call genWindowsMakefiles
+
+cd BasicUsageEnvironment
+del *.obj *.lib
+nmake /B -f BasicUsageEnvironment.mak
+
+cd ..\groupsock
+del *.obj *.lib
+nmake /B -f groupsock.mak
+
+cd ..\liveMedia
+del *.obj *.lib
+nmake /B -f liveMedia.mak
+
+cd ..\UsageEnvironment
+del *.obj *.lib
+nmake /B -f UsageEnvironment.mak
+
+cd ..\mediaServer
+del *.obj *.lib
+nmake /B -f mediaServer.mak
+
+pause
+
+:::::::::::::::::::::::::::::::::::::
